121d511f6656873b985563c2e43e27756b52ca67
Alex Carsello <alex.carsello@gmail.com>
2019-02-26 12:06:07 -0800

    Manually insert pad filler

--- pnr/design_data/insert_pads.tcl
+++ pnr/design_data/insert_pads.tcl
@@ -8,17 +8,31 @@ create_cell {corner_rb} CORNER
 #exists pad_clk
 #exists pad_reset
 #exists pad_a
+set num_fill 12
+for {set i 0} {$i<$num_fill} {incr i} {
+    create_cell w_fill_$i FILLER
+}
 
 ######## NORTH
 create_cell {pad_vssio_n} IOVSS_NS
 create_cell {pad_vdd} CVDDVSS_NS 
 create_cell {pad_vddio_n} IOVDD_NS
+for {set i 0} {$i<$num_fill} {incr i} {
+    create_cell n_fill_$i FILLER
+}
 
 ######## SOUTH
 create_cell {pad_vssio_s} IOVSS_NS
 create_cell {pad_vss} CVDDVSS_NS
 create_cell {pad_vddio_s} IOVDD_NS
+for {set i 0} {$i<$num_fill} {incr i} {
+    create_cell s_fill_$i FILLER
+}
 
 ######## EAST
 #exists pad_x
 #exists pad_y
+
+for {set i 0} {$i<$num_fill+1} {incr i} {
+    create_cell e_fill_$i FILLER
+}
