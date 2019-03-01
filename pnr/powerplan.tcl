2bbf0c9537a9e5c2c09c3846a2bd5a854c60e850
Alex Carsello <alex.carsello@gmail.com>
2019-02-28 16:48:20 -0800

    Remove manual power planning

--- pnr/powerplan.tcl
+++ pnr/powerplan.tcl
@@ -1,23 +1,3 @@
-##### Manual powerplanning approach using template file #####
-#create_rectangular_rings -nets {VDD VSS} -left_offset 5 -left_segment_layer M9 -left_segment_width 4 -right_offset 5 -right_segment_layer M9 -right_segment_width 4 -bottom_offset 5 -bottom_segment_layer M8 -bottom_segment_width 2 -top_offset 5 -top_segment_layer M8 -top_segment_width 2
-#
-#set_power_plan_strategy core \
-#-nets {VDD VSS} \
-#-core \
-#-template saed_32nm.tpl:m89_mesh(2.0,2.0) \
-#-extension {stop: outermost_ring}
-#
-##set_power_plan_strategy core_m3 \
-##-nets {VDD VSS} \
-##-core \
-##-template saed_32nm.tpl:m3_mesh(0.5) \
-##-extension {stop: design_boundary}
-##
-#
-#compile_power_plan
-# LCM of M8 and M9 pitch
-
-
 ##### Automatic power planning approach using power constraints #####
 set pitch 2.432
 
@@ -32,9 +12,9 @@ synthesize_fp_rail  -nets {VDD VSS} -voltage_supply 1.2 -synthesize_power_plan -
 commit_fp_rail
 
 derive_pg_connection -power_net $MW_POWER_NET -power_pin $MW_POWER_PORT -ground_net $MW_GROUND_NET -ground_pin $MW_GROUND_PORT
-#set_preroute_drc_strategy -min_layer M2 -max_layer MRDL
 
 ##### End #####
+#set_preroute_drc_strategy -min_layer M2 -max_layer MRDL
 # Connect power pads to ring... ignore this for now
 #preroute_instances -ignore_macros -ignore_cover_cells \
 #-connect_instances specified \
